f333e2e6557b17b9830af0b8e63c09537e35038d
Jimmy <jimmy.b.mau@gmail.com>
2023-08-22 09:54:25 -0400

    feat: add completed exercise 2

--- exer02/exer2.hs
+++ exer02/exer2.hs
@@ -21,3 +21,34 @@ join a (x:lst)  = x ++ a ++ join a lst
 fact' :: Int -> Int
 fact' a = foldl (\acc x -> acc * x) 1 [1..a]
 
+
+-- Hailstone sequence (from last week's exercises).
+hailstone :: Int -> Int
+hailstone n 
+    | even n    = n `div` 2
+    | otherwise = 3 * n  + 1
+
+
+{- Here's a broken-up implementation for clarity
+ -
+-- Tail Recursive Hailstone
+hailTail :: Int -> Int -> Int
+hailTail acc 1 = acc
+hailTail acc n = hailTail (acc + 1) (hailstone n)
+
+
+-- Why does this work like `hailLen n = hailTail 0 n`?
+-- A function that take two arguments is really a function that applies the first 
+-- argument to itself, returning a function, then applying the second argument to 
+-- the new function.
+-- Thus, hailTail 0 returns a function that is exactly what we want for hailLen.
+hailLen :: Int -> Int
+hailLen = hailTail 0
+
+-}
+
+hailLen :: Int -> Int
+hailLen n = hailTail 0 n
+    where
+        hailTail a 1 = a
+        hailTail a n = hailTail (a + 1) (hailstone n)
