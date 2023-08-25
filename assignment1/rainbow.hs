8597f81dd60dc10053cb119d7316f4ed0cd0a491
Jimmy <jimmy.b.mau@gmail.com>
2023-08-25 01:44:54 -0400

    doc: fix comments

--- assignment1/rainbow.hs
+++ assignment1/rainbow.hs
@@ -15,7 +15,7 @@ height = 10000
 filename = "table.txt"
 
 
--- Takes a hash in base 10 and converts to base n.
+-- Converts hash from base 10 to base n.
 -- e.g. 1726491528 (base 10 integer) -> 12013440212103 (base 5 integer)
 convertBase :: Int -> Hash -> [String]
 convertBase 0 _       = ["0"]
@@ -25,7 +25,7 @@ convertBase base hash = show remainder : convertBase base quotient
         quotient = hash `div` fromIntegral base
 
 
--- Takes string in base n and takes <length> least significant digits.
+-- Takes <length> least significant digits from string.
 -- e.g. 12013440212103 -> 40212103
 lenLeastSig :: Int -> [String] -> [String]
 lenLeastSig len xs = reverse (take len xs)
@@ -37,7 +37,7 @@ digToLet :: [String] -> Passwd
 digToLet = map (toLetter . read) 
 
 
--- Composes all the previous functions together.
+-- Reduces hash back to another password.
 pwReduce :: Hash -> Passwd
 pwReduce = digToLet . lenLeastSig pwLength . convertBase nLetters 
 
@@ -63,7 +63,6 @@ generateTable = do
 
 
 -- Find column in rainbow table.
--- Note: Only called if findChain finds a match.
 findNode :: Int -> Hash -> Maybe Passwd -> Maybe Passwd
 findNode _ _ Nothing = Nothing
 findNode w hash (Just pass)
@@ -86,8 +85,6 @@ findChains table count hash  = filter (/= Nothing) (checkRows count hash)
 
 
 -- Safely grab head from a [Maybe a] list type.
--- There is not way of safely grabbibg head. Will lead to a runtime error if list is empty.
--- This adds extra pattern of distinguishing Nothing from Just.
 -- There is no way of safely grabbing head. Throws a runtime error if list is empty.
 -- This function adds extra pattern of distinguishing Nothing from Just.
 maybeHead :: [Maybe a] -> Maybe a
@@ -98,10 +95,10 @@ maybeHead (Just a:_) = Just a
 
 -- Tries to find password in rainbow table.
 findPassword :: Map.Map Hash Passwd -> Int -> Hash -> Maybe Passwd
-findPassword table w hash = mySafeHead $ map (findNode w hash) (findChains table w hash) 
+findPassword table w hash = maybeHead $ map (findNode w hash) (findChains table w hash) 
 
 
--- Tests pre-generated against n number of passwords.
+-- Tests pre-generated table against n randomly-generated passwords.
 test2 :: Int -> IO ([Passwd], Int)
 test2 n = do
   table <- readTable filename
